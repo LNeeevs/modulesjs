4f38a24f390eece3014a9d789f76431e468c3c1d
LNeeevs <lucasneves.dev@outlook.com>
2019-06-17 16:53:58 -0300

    ESLINT instalado e configurado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 const returned = (a,b) => { console.log(a+b) }
 
 returned(10,20);
+
+const farenheit = [10,20,30,40,50,60,70,80,90,100];
+
+const transformCelsium = () => {
+    return farenheit.map(farenheit => Math.round(((farenheit-32)*5/9)));
+}
+
+console.log(transformCelsium());
